b413517ac62c9667254892d2c2ae2958a2337121
idok <1541115709@qq.com>
2022-07-13 17:11:24 +0800

    通用参数测试通过,增加用例

--- bellman/src/groth16/mpc_test.rs
+++ bellman/src/groth16/mpc_test.rs
@@ -6,9 +6,12 @@ mod mpc_tests {
     use bls12_381::{Bls12, Scalar};
     #[test]
     fn common_works() {
+        //验证通用系数
         let g1 = bls12_381::G1Affine::generator();
         let g2 = bls12_381::G2Affine::generator();
-        let mut paramter_in_storage = initial_common_paramters::<Bls12>(4);
+
+        let mut paramter_in_storage =
+            initial_common_paramters::<Bls12>(4 /*这个数表示最高到x的几次幂*/);
         //under-chain
         let player1_common = mpc_common_paramters_generator(&paramter_in_storage, (1, 2, 3));
         //on-chain
@@ -32,28 +35,41 @@ mod mpc_tests {
         let alpha_mul_tau_g2 = paramter_in_storage.alpha_mul_tau_g2;
         let beta_mul_tau_g1 = paramter_in_storage.beta_mul_tau_g1;
         let beta_mul_tau_g2 = paramter_in_storage.beta_mul_tau_g2;
+
         assert_eq!(alpha_g1, g1 * Scalar::from(6));
         assert_eq!(alpha_g2, g2 * Scalar::from(6));
         assert_eq!(beta_g1, g1 * Scalar::from(24));
         assert_eq!(beta_g2, g2 * Scalar::from(24));
+        assert_eq!(tau_g1.len(), 4);
         assert_eq!(tau_g1[0], g1 * Scalar::from(60));
         assert_eq!(tau_g2[0], g2 * Scalar::from(60));
         assert_eq!(tau_g1[1], g1 * Scalar::from(3600));
         assert_eq!(tau_g2[1], g2 * Scalar::from(3600));
         assert_eq!(tau_g1[2], g1 * Scalar::from(216000));
         assert_eq!(tau_g2[2], g2 * Scalar::from(216000));
+        assert_eq!(tau_g1[3], g1 * Scalar::from(12960000));
+        assert_eq!(tau_g2[3], g2 * Scalar::from(12960000));
+        assert_eq!(alpha_mul_tau_g1.len(), 4);
         assert_eq!(alpha_mul_tau_g1[0], g1 * Scalar::from(60 * 6));
         assert_eq!(alpha_mul_tau_g2[0], g2 * Scalar::from(60 * 6));
         assert_eq!(alpha_mul_tau_g1[1], g1 * Scalar::from(3600 * 6));
         assert_eq!(alpha_mul_tau_g2[1], g2 * Scalar::from(3600 * 6));
         assert_eq!(alpha_mul_tau_g1[2], g1 * Scalar::from(216000 * 6));
         assert_eq!(alpha_mul_tau_g2[2], g2 * Scalar::from(216000 * 6));
+        assert_eq!(alpha_mul_tau_g1[3], g1 * Scalar::from(12960000 * 6));
+        assert_eq!(alpha_mul_tau_g2[3], g2 * Scalar::from(12960000 * 6));
+        assert_eq!(beta_mul_tau_g1.len(), 4);
         assert_eq!(beta_mul_tau_g1[0], g1 * Scalar::from(60 * 24));
         assert_eq!(beta_mul_tau_g2[0], g2 * Scalar::from(60 * 24));
         assert_eq!(beta_mul_tau_g1[1], g1 * Scalar::from(3600 * 24));
         assert_eq!(beta_mul_tau_g2[1], g2 * Scalar::from(3600 * 24));
         assert_eq!(beta_mul_tau_g1[2], g1 * Scalar::from(216000 * 24));
         assert_eq!(beta_mul_tau_g2[2], g2 * Scalar::from(216000 * 24));
+        assert_eq!(beta_mul_tau_g1[3], g1 * Scalar::from(12960000 * 24));
+        assert_eq!(beta_mul_tau_g2[3], g2 * Scalar::from(12960000 * 24));
+
+        //验证matrix
+        //let at_aux = vec![];
     }
 
     #[test]
